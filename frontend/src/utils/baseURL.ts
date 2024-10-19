f5a78b59dfb2b99ebf960162695fd27dc105dd73
longmaker2 <l.deng@alustudent.com>
2024-10-19 21:23:36 +0200

    updated baseURL

--- frontend/src/utils/baseURL.ts
+++ frontend/src/utils/baseURL.ts
@@ -1,3 +1,3 @@
 // export const baseURL = 'http://localhost:5000/api';
 
-export const baseURL = "https://community-connect-s2lw.onrender.com/api";
+export const baseURL = "https://community-connect-wp5s.onrender.com";
